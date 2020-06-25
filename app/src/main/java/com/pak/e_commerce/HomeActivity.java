2a23f70330461cea96cabc9e6f04ec7416eb016b
quick <quicklaunchers1@gmail.com>
2020-06-25 17:13:50 +0500

    search

--- app/src/main/java/com/pak/e_commerce/HomeActivity.java
+++ app/src/main/java/com/pak/e_commerce/HomeActivity.java
@@ -218,6 +218,14 @@ public class HomeActivity extends AppCompatActivity
                 startActivity(intent);
             }
         }
+        else if (id == R.id.nav_search)
+        {
+            if (!type.equals("Admin"))
+            {
+                Intent intent = new Intent(HomeActivity.this, SearchProductsActivity.class);
+                startActivity(intent);
+            }
+        }
 
         else if (id == R.id.nav_categories)
         {
